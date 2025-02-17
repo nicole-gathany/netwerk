849fe0277d3f0e696597f750b0a48e74585501e8
Nicole <nicole.gathany@gmail.com>
2025-02-16 23:40:15 -0500

    add interface

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,7 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+    readonly VITE_APPWRITE_PROJECT_ID: string
+    readonly VITE_APPWRITE_DATABASE_ID: string
+    readonly VITE_APPWRITE_COLLECTION_ID: string
+}
