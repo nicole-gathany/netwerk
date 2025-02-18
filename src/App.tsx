e3b379264933f9a09d2d08b5909bb5f9cccc4918
Nicole <nicole.gathany@gmail.com>
2025-02-18 01:47:40 -0500

    fix small typo

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ export default function App(){
   return (
     <div>
       <h1>Netwerk!</h1>
-      <p>The purpose of Netwerk! is to gamify and humanize the networking experience by providing prompts, setting scavenger-hunt like challenges, and awarding badges during networking events and meetups. Check out the proof of concept below.</p>
+      <p>The purpose of Netwerk! is to gamify and humanize the networking experience by providing prompts, setting scavenger-hunt-like challenges, and awarding badges during networking events and meetups. Check out the proof of concept below.</p>
       <Prompt></Prompt>
       <NetworkingForm></NetworkingForm>
       <ShowContacts></ShowContacts>
