b74205ede91690536557b60aa4b1c55e159752e1
Nicole <nicole.gathany@gmail.com>
2025-02-18 00:56:43 -0500

    meta environment

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -4,4 +4,5 @@ interface ImportMetaEnv {
     readonly VITE_APPWRITE_PROJECT_ID: string
     readonly VITE_APPWRITE_DATABASE_ID: string
     readonly VITE_APPWRITE_COLLECTION_ID: string
+    readonly VITE_APPWRITE_PROMPTS_COLLECTION_ID: string
 }
